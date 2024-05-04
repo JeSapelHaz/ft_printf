d3be49e8ba7ce84f6c13392ae623a6b4af450ca0
Hasnain Butt <hbutt@shi-c1-p4.s19.be>
2024-05-04 16:32:10 +0200

    printf

--- ft_format.c
+++ ft_format.c
@@ -6,13 +6,13 @@
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 20:54:57 by hbutt             #+#    #+#             */
-/*   Updated: 2024/04/28 21:33:17 by hbutt            ###   ########.fr       */
+/*   Updated: 2024/05/03 17:16:42 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* cspdiuxX% 
+/* cspdiuxX%
 	c : char
 	s : string
 	p : pointeur
@@ -24,31 +24,34 @@
 	*/
 size_t	ft_format(va_list args, char c)
 {
-	size_t result;
+	size_t	result;
 
 	result = 0;
 	if (c == 'c')
 		result = result + ft_putchar(va_arg(args, int));
 	if (c == 's')
-		result = result + 
-	if (c == 'd')
-		result = result + 
+		result = result + ft_putstr(va_arg(args, char *));
+	if (c == 'd' || c == 'i')
+		result = result + ft_nbr(va_arg(args, int));
 	if (c == '%')
 		result = result + ft_putchar('%');
 	if (c == 'p')
-		result = result + 
+		result = result + ft_putstr("0x") + ft_putnbr_base(va_arg(args,
+					unsigned), "0123456789abcdef");
 	if (c == 'u')
-		result = result + 
+		result = result + ft_putnbr_base(va_arg(args, unsigned), "0123456789");
 	if (c == 'x')
-		result = result + 
+		result = result + ft_putnbr_base(va_arg(args, unsigned),
+				"0123456789abcdef");
 	if (c == 'X')
-		result = result +
+		result = result + ft_putnbr_base(va_arg(args, unsigned),
+				"0123456789ABCDEF");
 	return (result);
 }
 
 size_t	ft_putnbr(long int nbr)
 {
-	size_t result;
+	size_t	result;
 
 	result = 0;
 	if (nbr < 0)
@@ -58,12 +61,26 @@ size_t	ft_putnbr(long int nbr)
 	}
 	if (nbr > 9)
 	{
-		result = result + ft_putnbr(nbr/10);
-		result = result + ft_putnbr(nbr%10)
+		result = result + ft_putnbr(nbr / 10);
+		result = result + ft_putnbr(nbr % 10);
 	}
+	else
+		result = result + ft_putchar(nbr + 48);
+	return (result);
 }
 
-size_t ft_putnbr_base(unsigned long int nbr, const char *base)
+size_t	ft_putnbr_base(unsigned long int nbr, const char *base)
 {
-	
+	size_t 	len_base;
+	size_t 	result;
+
+	len_base = ft_strlen(base);
+	if (nbr < len_base)
+		result = ft_putchar(base[nbr])
+	if (nbr >= len_base)
+	{
+		result = result + ft_putnbr_base(nbr / len_base, base);
+		result = result + ft_putnbr_base(nbr % len_base, base);
+	}
+	return (result);
 }
--- ft_printf.c
+++ ft_printf.c
@@ -6,19 +6,19 @@
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 18:31:13 by hbutt             #+#    #+#             */
-/*   Updated: 2024/04/28 20:52:46 by hbutt            ###   ########.fr       */
+/*   Updated: 2024/05/03 17:23:05 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char* format, ...)
+int	ft_printf(const char *format, ...)
 {
 	size_t result;
 	va_list args;
 
 	va_start(args, format);
-	
+
 	va_end(args);
-	return result;
+	return (result);
 }
